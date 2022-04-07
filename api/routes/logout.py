f0275615a86c0f63af403a08c915815439312273
frank <oprel.fj@gmail.com>
2022-04-07 15:25:04 +0200

    Implemented logout

--- api/routes/logout.py
+++ api/routes/logout.py
@@ -1,8 +1,29 @@
-from flask import Blueprint
-
+from datetime import datetime
+from flask import Blueprint, request
+from ..helpers.authentication import token_required
+from ..models.users import User, TokenBlocklist
 
 logout = Blueprint('logout', __name__)
 
 @logout.route('/users/logout', methods=['POST'])
-def route():
-    return {}, 200
+@token_required
+def route(user):
+
+    # Disable user token
+    user.jwt_auth = False
+    user.save()
+
+    # Revoke user token
+    token = request.headers['authorization']
+
+    blocklist = TokenBlocklist(
+        token = token,
+        created_at = datetime.utcnow()
+    )
+
+    blocklist.save()
+
+    return {
+        'success': True,
+        'message': 'User logged out' 
+    }, 200
